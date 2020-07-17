8f362f4e0810fc26dd89bebe43b9fb3b76d30e62
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-07-17 15:56:07 +0100

    rename request

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = '24d6427f262bac6aa3746e79241ac088';
 
 export const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrendingNow: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
