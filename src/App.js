b9aa0e60bbf84a5de9ccc9998b4d0bcf581cef47
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-07-19 08:04:26 +0100

    plug in the built components into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import { requests } from './utils/requests';
+
+import Navbar from './components/navbar/Navbar'
+import Row from './components/row/Row';
+import Banner from './components/banner/Banner';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to MovNet!!!</h1>
+    <div className="app">
+      <Navbar />
+      <Banner />
+      <Row
+        title="MOVNET ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrendingNow} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
